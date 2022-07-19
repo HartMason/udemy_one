e24dccaba6b3881f7a7a1294dd38ece794a8d5be
HartMason <hartmason85@gmail.com>
2022-07-19 09:59:05 -0500

    updated imports after re-organizing files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css'
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
